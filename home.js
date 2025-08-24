aee7124fee9bf25dc706de59b88ba8c268e44545
mjh-shikder <mjhsikder@gmail.com>
2025-08-24 23:30:37 +0600

    add money and cashout widget click action added

--- home.js
+++ home.js
@@ -1,14 +1,26 @@
+const validPin = 12345
+
 document.getElementById('btn-add-money')
     .addEventListener('click', function (e) {
         e.preventDefault()
         
         const bank = document.getElementById('bank').value;
-        const accountNumber = parseInt(document.getElementById('account-number').value);
+        const accountNumber = document.getElementById('account-number').value;
         const addAmount = parseInt(document.getElementById('add-amount').value);
         const pin = parseInt(document.getElementById('add-pin').value) ;
 
         const availableBlance = parseInt(document.getElementById('available-blance')
         .innerText)
+
+        if (accountNumber.length < 11) {
+            alert('Provide a valid account number to continue')
+            return;
+        }
+
+        if (validPin !== pin) {
+            alert('Pin number is not correct')
+            return;
+        }
 
         const totalBlance = availableBlance + addAmount;
         
@@ -16,4 +28,29 @@ document.getElementById('btn-add-money')
 
         document.getElementById('available-blance').innerText = totalBlance
         
-})
+    })
+
+
+// Toggling features
+document.getElementById('w-add-money')
+    .addEventListener('click', function () {
+        document.getElementById('cashout-parent').style.display = 'none';
+
+        document.getElementById('add-money-parent').style.display = 'block';
+        
+    })
+    
+    
+    document.getElementById('w-cashout')
+        .addEventListener('click', function () {
+            document.getElementById('add-money-parent').style.display = 'none';
+
+            document.getElementById('cashout-parent').style.display = 'block';
+                
+            
+            
+            
+    
+            
+        })
+
