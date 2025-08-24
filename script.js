523066f9d40c3611509be8edc21f5f65a0358052
mjh-shikder <mjhsikder@gmail.com>
2025-08-24 16:51:04 +0600

    Login page js added

---
+++ script.js
@@ -0,0 +1,23 @@
+// Login button 
+document.getElementById('btn-login')
+    .addEventListener('click', function (e) {
+        e.preventDefault
+        const mobileNumber = 12345678910;
+        const pinNumber = 12345;
+        const mobileNumberValue = document.getElementById('mobile-number').value;
+        const mobileNumberValueConverted = parseInt(mobileNumberValue);
+        const pinNumberValue = document.getElementById('pin-number').value;
+        const pinNumberValueConverted = parseInt(pinNumberValue);
+
+        console.log(mobileNumberValueConverted, pinNumberValueConverted);
+        
+        if (mobileNumberValueConverted === mobileNumber
+            && pinNumberValueConverted === pinNumber) {
+            console.log('all values matched');
+            
+            }
+        else {
+            console.log('invalid credentials');
+            
+            }
+})      
