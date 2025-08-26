ffaecdbd3e9d209bf31e570e48229961e895b83d
mjh-shikder <mjhsikder@gmail.com>
2025-08-26 17:03:33 +0600

    overall toggle funcion refined

--- home.js
+++ home.js
@@ -15,14 +15,14 @@ function getInputValue(id) {
 document.getElementById('btn-add-money')
     .addEventListener('click', function (e) {
         e.preventDefault()
-        
+
         const bank = getInputValue('bank');
         const accountNumber = document.getElementById('account-number').value;
         const addAmount = getInputValueNumber('add-amount');
         const pin = getInputValueNumber('add-pin');
 
         const availableBlance = parseInt(document.getElementById('available-blance')
-        .innerText)
+            .innerText)
 
         if (accountNumber.length < 11) {
             alert('Provide a valid account number to continue')
@@ -35,56 +35,92 @@ document.getElementById('btn-add-money')
         }
 
         const totalBlance = availableBlance + addAmount;
-        
+
         // Now set the new amout to the page
 
         document.getElementById('available-blance').innerText = totalBlance
-        
+
     })
 
 
 // cashout money feature
-document.getElementById('btn-withdraw').addEventListener('click', function(e) {
+document.getElementById('btn-withdraw').addEventListener('click', function (e) {
     e.preventDefault
     const withdrawAmount = getInputValueNumber('withdraw-amount');
     const availableBlance = parseInt(document.getElementById('available-blance')
         .innerText)
 
     const totalNewAvailableBlance = availableBlance - withdrawAmount;
-    
+
     document.getElementById('available-blance').innerText = totalNewAvailableBlance;
-    
-    
+
+
 })
 
 
 
 // Toggling features
+// w add money
 document.getElementById('w-add-money')
     .addEventListener('click', function () {
-        document.getElementById('cashout-parent').style.display = 'none';
-        document.getElementById('send-money-parent').style.display = 'none';
-
+        const forms = document.getElementsByClassName('form');
+        for (const form of forms) {
+            form.style.display = 'none';
+        }
         document.getElementById('add-money-parent').style.display = 'block';
-        
+
     })
-    
-    
-    document.getElementById('w-cashout')
-        .addEventListener('click', function () {
-            document.getElementById('add-money-parent').style.display = 'none';
-            document.getElementById('send-money-parent').style.display = 'none';
 
-            document.getElementById('cashout-parent').style.display = 'block';
-            
-        })
+// w chasout
+document.getElementById('w-cashout')
+    .addEventListener('click', function () {
+        const forms = document.getElementsByClassName('form');
+        for (const form of forms) {
+            form.style.display = 'none';
+        }
 
+        document.getElementById('cashout-parent').style.display = 'block';
+
+    })
+// w send money
 document.getElementById('w-send-money').addEventListener('click', function () {
-    document.getElementById('add-money-parent').style.display = 'none';
-    document.getElementById('cashout-parent').style.display = 'none';
+
+
+    const forms = document.getElementsByClassName('form');
+    for (const form of forms) {
+        form.style.display = 'none';
+    }
     document.getElementById('send-money-parent').style.display = 'block';
+
 })
 
+//w get bonus
+document.getElementById('w-get-bonus').addEventListener('click', function () {
+    
+      const forms = document.getElementsByClassName('form');
+    for (const form of forms) {
+        form.style.display = 'none';
+    }
+    document.getElementById('get-bonus-parent').style.display = 'block';
+})
 
+// w pay bill
+document.getElementById('w-pay-bill').addEventListener('click', function () {
+    
+      const forms = document.getElementsByClassName('form');
+    for (const form of forms) {
+        form.style.display = 'none';
+    }
+    document.getElementById('pay-bill-parent').style.display = 'block';
+})
 
+// w transaction
+document.getElementById('w-transaction').addEventListener('click', function () {
+    
+      const forms = document.getElementsByClassName('form');
+    for (const form of forms) {
+        form.style.display = 'none';
+    }
+    document.getElementById('transaction-parent').style.display = 'block';
+})
 
